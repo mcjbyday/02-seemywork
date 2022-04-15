91b63a9ed54d6997ed3cdddb0a7540b96cabf6e8
MCJBYDAY <jacobsmichaelc@gmail.com>
2022-04-15 16:07:40 -0500

    added profile picture toggling and hovering

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,35 @@
-$('#img_hover').attr("src", "./assets/css/images/figures/164998400323332140.jpg");
-
 getMet();
+
+// $('#img_hover').attr("src", "./assets/css/images/figures/164998400323332140.jpg");
+
+var faceOn = false;
+
+$('#avatar_img').hover( 
+    revealFace
+    , returnBlur
+);
+
+function revealFace() {
+    $(this).attr({"style": "filter: none;opacity: 1","src":"./assets/css/images/figures/img_0694682396022516.png"})
+}
+
+function returnBlur() {
+    if (faceOn === false) {
+        $('#avatar_img').attr({"src":"./assets/css/images/figures/164998400323332140.jpg","style":"filter: blur(0.2rem);opacity: 85%;"});
+    }
+}
+
+function faceOnSwitch() {
+    if (faceOn === true) {
+        faceOn = false;
+    }
+    else {
+        faceOn = true;
+    }
+}
+
+$('#avatar_img').on("click", faceOnSwitch);
+
 
 function getMet() {
 
@@ -19,4 +48,25 @@ function getMet() {
     })
 };
             
+
+
+
+// $('#avatar_img').hover(function() {
+//     if (faceOn) {
+//         $(this).attr({"src":"./assets/css/images/figures/img_0694682396022516.png","style":"filter: none;opacity: 1"})
+//     }
+//     else {$(this).attr({"src":"./assets/css/images/figures/164998400323332140.jpg","style":"filter: blur(0.2rem);opacity: 85%;"})
+//     }
+//     // , function() {
+//     // if (faceOn) 
+//     // $(this).attr({"src":"./assets/css/images/figures/164998400323332140.jpg","style":"filter: blur(0.2rem);opacity: 85%;"})
+// });
+
+// $('#avatar_img').taphold(function() {
+//     $(this).attr({"src":"./assets/css/images/figures/img_0694682396022516.png","style":"filter: none;opacity: 1"})}
+    
+//     , function() {
+//         $(this).attr({"src":"./assets/css/images/figures/164998400323332140.jpg","style":"filter: blur(0.2rem);opacity: 85%;"})
+// });
+
 
